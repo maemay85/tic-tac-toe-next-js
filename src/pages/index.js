be8749718635fabc37c8e609c54868f025723b68
maemay85 <hellomaemay@gmail.com>
2023-03-22 12:35:09 -0400

    Cleared out template CSS and wrote JSX for Game and GameBoard

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,26 @@ import styles from '@/styles/Home.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const gameBoard = [...Array(9).map((v, idx) => {
+    return (
+      <div key={idx} className="square">
+        X
+      </div>
+    );
+  })]
+
   return (
     <div>
       <h1>Tic Tac Toe</h1>
+      <div className='game'>
+        <div className='game_menu'>
+          <p>xTurn</p>
+        </div>
+        <div className='game_board'>
+          {gameBoard}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
